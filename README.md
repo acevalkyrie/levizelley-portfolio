44e4c7e74a519efaba24b2044792647045c2e2f1
Jesse Sordyl <jessesordyl@acevalkyrie.dev>
2026-01-04 19:18:16 -0500

    Add initial site files

---
+++ README.md
@@ -0,0 +1,16 @@
+# AceValkyrie Web Portfolio Version 0.1
+Portfolio web site for Levi Zelley under the branding of AceValkyrie. Displays important information for potential employers such as resume, education details, projects undertaken, and certifications.
+Created by Levi Zelley on DATE.
+
+## Site Use
+The site is intended to be found at https://levizelley.aevalkyrie.dev/ and is a simple web page accessible by anyone.  
+Functionality includes jump links to get to individual sections, site color theme settings, and pure CSS carousels to tab through different information.
+
+## Upcoming Changes
+ - Complete site overhaul
+  - CSS needs to be more clearly organized
+  - ID and class names need to be clearer
+  - Load times are unacceptable
+ - SEO optimization
+
+**Noticed a bug, have a suggestion, or want to work with me? Message me at [AceValkyrieDev](https://twitter.com/AceValkyreDev) or using the contact info on my resume.**
---
+++ js/index.js
@@ -0,0 +1,58 @@
+const imgCheckboxes = document.querySelectorAll('.caritem input[type="checkbox"]');
+let backFill = document.getElementById("uncheck");
+let fullImage = document.getElementById("fullscreen")
+
+for (i of imgCheckboxes) {
+    i.addEventListener("click", setFullScreen)
+}
+
+function setFullScreen() {
+    for (i of imgCheckboxes) {
+        if (i.checked) {
+            let style = window.getComputedStyle(i);
+            let bgImg = style.backgroundImage;
+            
+            backFill.classList.remove("atrest")
+            backFill.classList.add("coverscreen")
+            fullImage.classList.remove("atrest")
+            fullImage.classList.add("coverscreen")
+            fullImage.style.backgroundImage = bgImg;
+        }
+    }
+}
+
+backFill.addEventListener("click", unsetFullScreen)
+
+function unsetFullScreen() {
+    fullImage.style.backgroundImage = null;
+    backFill.classList.add("atrest")
+    backFill.classList.remove("coverscreen")
+    fullImage.classList.add("atrest")
+    fullImage.classList.remove("coverscreen")
+    for (i of imgCheckboxes) {
+        if (i.checked) {
+            i.checked = false;
+        }
+    }
+}
+
+
+/**** POSSIBLE FRAMEWORK FOR STORING EXTRANEOUS CONTENT ELSEWHERE UNTIL IT NEEDS TO LOAD, CUTTING DOWN ON LOADING TIME ****/
+/* const projects = document.getElementById("projectcontainer");
+
+const url = "js/test.html";
+
+async function inserttext(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const text = await response.text();
+        projects.innerHTML = text;
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+inserttext(url); */
